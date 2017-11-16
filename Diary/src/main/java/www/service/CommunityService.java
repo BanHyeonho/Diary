47a972f9cd88b7a243f91ee0ed917620c3961cb9
user1 <user1@user1-PC>
2017-11-16 15:13:30 +0900

    2017-11-16 만남의장 댓글 김형우

--- Diary/src/main/java/www/service/CommunityService.java
+++ Diary/src/main/java/www/service/CommunityService.java
@@ -1,14 +1,18 @@
 package www.service;
 
+import java.util.List;
+
+import www.dto.CommentVo;
 import www.dto.CommunityVo;
 
 public interface CommunityService {
 	public void communityinsert(CommunityVo vo);//만남의 장 글쓰기
 //	public List<CommunityVo> csearch(CommunityVo vo);	//만남의장 검색
 	public CommunityVo oneCommunity(int idx); 	//만남의 장 글 하나보기
-//	public void writeccomment(CommentVo vo);//만남의장에 댓글쓰기
+	public void insertccomment(CommentVo vo);//만남의장에 댓글쓰기
 //	public void deleteccomment(int idx);	//만남의장 댓글삭제
 //	public void updateccomment(CommentVo vo);//만남의장 댓글수정
+	public List<CommentVo> ccomment(int idx);
 	
 	
 	
